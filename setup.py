383f25360341e945086e584dcdb7420f2a76ca88
Argoneon1810 <jinhoon.choi.97@gmail.com>
2026-01-05 06:16:02 +0000

    open(".") error fixed

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="circular-buffer",
     version="0.1.0",
     description="A flexible Circular Buffer implementation supporting both PyTorch and NumPy backends automatically.",
-    long_description=open("README.md").read() if "README.md" in open(".").read() else "",
+    long_description=open("README.md", encoding="utf-8").read() if os.path.exists("README.md") else "",
     long_description_content_type="text/markdown",
     author="Argoneon1810",
     author_email="jinhoon.choi.97@gmail.com",
